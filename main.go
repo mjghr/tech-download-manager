66f2865dbef081eba49c6a1999cb9cb211c14466
mjghr <mjghrwork@gmail.com>
2025-03-04 11:20:19 +0330

    added util package and const.go to load env vars and access them globally. also added a test fucntion to get user input

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
-	"github.com/joho/godotenv"
+	"net/url"
 	"os"
+	"github.com/mjghr/tech-download-manager/util"
 )
 
 func main(){
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("failed loading env file")
-	}
-
+	util.LoadEnv();
 	welcomeMessage := os.Getenv("WELCOME_MESSAGE")
 	fmt.Println(welcomeMessage)
 
+	url, err := GetUrlFromUser()
+	if err != nil {
+		log.Fatal("invalid URL:", err)
+	}
+
+	fmt.Println(url)
+}
+
+
+func GetUrlFromUser()(*url.URL, error){
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter the file URL to download: ")
+	scanner.Scan()
+	userInput := scanner.Text()
+	parsedURL, err := url.Parse(userInput)
+	if err != nil {
+		return nil, err
+	}
+
+	return parsedURL, nil
 }
