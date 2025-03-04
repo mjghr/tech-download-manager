4fe38b568b36e8d15da358cfba9143d328c05b41
mjghr <mjghrwork@gmail.com>
2025-03-04 11:20:19 +0330

    added util package and const.go to load env vars and access them globally. also added a test fucntion to get user input

---
+++ util/const.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+var (
+	WELCOME_MESSAGE     string
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file.")
+	}
+	WELCOME_MESSAGE = getEnv("WELCOME_MESSAGE", "8080")
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
