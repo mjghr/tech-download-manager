763045f66c1bf08b3d7c94a3b2d29712f1726d32
mjghr <mjghrwork@gmail.com>
2025-03-08 12:57:42 +0330

    done with simple download process and tested it

--- controller/download_controller.go
+++ controller/download_controller.go
@@ -8,12 +8,8 @@ import (
 
 	"github.com/mjghr/tech-download-manager/client"
 	"github.com/mjghr/tech-download-manager/config"
-	"github.com/mjghr/tech-download-manager/models"
 )
 
-type DownloadController struct {
-	model *models.DownloadController
-}
 
 type DownloadRequest struct {
 	Url        string
@@ -24,10 +20,9 @@ type DownloadRequest struct {
 	HttpClient *client.HTTPClient
 }
 
-
 func (d *DownloadRequest) SplitIntoChunks() [][2]int {
 	arr := make([][2]int, d.Chunks)
-	for i := 0; i < d.Chunks; i++ {
+	for i := range d.Chunks {
 		if i == 0 {
 			arr[i][0] = 0
 			arr[i][1] = d.ChunkSize
@@ -39,13 +34,11 @@ func (d *DownloadRequest) SplitIntoChunks() [][2]int {
 			arr[i][1] = arr[i][0] + d.ChunkSize
 		}
 	}
-
 	return arr
 }
 
 func (d *DownloadRequest) Download(idx int, byteChunk [2]int) error {
 	log.Printf("Downloading chunk %v", idx)
-	// make GET request with range
 	method := "GET"
 	headers := map[string]string{
 		"User-Agent": "CFD Downloader",
@@ -55,68 +48,54 @@ func (d *DownloadRequest) Download(idx int, byteChunk [2]int) error {
 	if err != nil {
 		return fmt.Errorf("chunk fail %v", resp.StatusCode)
 	}
-
 	if resp.StatusCode > 299 {
 		return fmt.Errorf("can't process, response is %v", resp.StatusCode)
 	}
-
-	// open a file to write the body to
 	fileName := fmt.Sprintf("%v-%v.tmp", config.TMP_FILE_PREFIX, idx)
 	file, err := os.Create(fileName)
 	if err != nil {
 		return fmt.Errorf("can't create a file %v", fileName)
 	}
 	defer file.Close()
-
-	// write to file
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
 		return fmt.Errorf("failed to write to file: %v", err)
 	}
 	log.Printf("Wrote chunk %v to file", idx)
-
 	return nil
 }
 
 
 func (d *DownloadRequest) MergeDownloads() error {
-	// create output file
 	out, err := os.Create(d.FileName)
 	if err != nil {
 		return fmt.Errorf("failed to create output file: %v", err)
 	}
 	defer out.Close()
-
-	// append each chunk to final file
 	for idx := 0; idx < d.Chunks; idx++ {
 		fileName := fmt.Sprintf("%v-%v.tmp", config.TMP_FILE_PREFIX, idx)
 		in, err := os.Open(fileName)
 		if err != nil {
-			return fmt.Errorf("Failed to open chunk file %s: %v", fileName, err)
+			return fmt.Errorf("failed to open chunk file %s: %v", fileName, err)
 		}
 		defer in.Close()
-
 		_, err = io.Copy(out, in)
 		if err != nil {
-			return fmt.Errorf("Failed to merge chunk file %s: %v", fileName, err)
+			return fmt.Errorf("failed to merge chunk file %s: %v", fileName, err)
 		}
 	}
-
-	fmt.Println("File chunks merged successfully...")
+	fmt.Println("file chunks merged successfully...")
 	return nil
 }
 
 func (d *DownloadRequest) CleanupTmpFiles() error {
 	log.Println("Starting to clean tmp downloaded files...")
-
-	// delete each chunk file
-	for idx := 0; idx < d.Chunks; idx++ {
+	for idx := range d.Chunks {
 		fileName := fmt.Sprintf("%v-%v.tmp", config.TMP_FILE_PREFIX, idx)
 		err := os.Remove(fileName)
 		if err != nil {
-			return fmt.Errorf("Failed to remove chunk file %s: %v", fileName, err)
+			return fmt.Errorf("failed to remove chunk file %s: %v", fileName, err)
 		}
 	}
-
 	return nil
 }
--- manager/download_manager.go
+++ manager/download_manager.go
@@ -4,8 +4,11 @@ import (
 	"log"
 	"net/url"
 	"strconv"
+	"fmt"
+	"sync"
+
 	"github.com/mjghr/tech-download-manager/client"
-	"github.com/mjghr/tech-download-manager/models"
+	"github.com/mjghr/tech-download-manager/controller"
 	"github.com/mjghr/tech-download-manager/util"
 )
 
@@ -21,6 +24,9 @@ func Download(urlPtr *url.URL) {
 		log.Fatal(err)
 	}
 
+	acceptRangers := response.Header.Get("Accept-Ranges")
+	log.Println(acceptRangers)
+
 	contentLength := response.Header.Get("Content-Length")
 	log.Println("Content-Length:", contentLength)
 	contentLengthInBytes, err := strconv.Atoi(contentLength)
@@ -35,8 +41,10 @@ func Download(urlPtr *url.URL) {
 	if err != nil {
 		log.Fatal("Error while extracting file name...")
 	}
+
+	
 	log.Println("Filename extracted: ", fileName)
-	downReq := &models.DownloadRequest{
+	downReq := &controller.DownloadRequest{
 		Url:        url,
 		FileName:   fileName,
 		Chunks:     workers,
@@ -45,5 +53,33 @@ func Download(urlPtr *url.URL) {
 		HttpClient: httpRequestSender,
 	}
 
+	byteRangeArray := make([][2]int, workers)
+	byteRangeArray = downReq.SplitIntoChunks()
+	fmt.Println(byteRangeArray)
+
+	var wg sync.WaitGroup
+	for idx, byteChunk := range byteRangeArray {
+		wg.Add(1) 
+		go func(idx int, byteChunk [2]int) {
+			defer wg.Done()
+			err := downReq.Download(idx, byteChunk)
+			if err != nil {
+				log.Fatal(fmt.Sprintf("Failed to download chunk %v", idx), err)
+			}
+		}(idx, byteChunk)
+	}
+	wg.Wait()
+
+	err = downReq.MergeDownloads()
+	if err != nil {
+		log.Fatal("Failed merging tmp downloaded files...", err)
+	}
+
+	err = downReq.CleanupTmpFiles()
+	if err != nil {
+		log.Fatal("Failed cleaning up tmp downloaded files...", err)
+	}
+
+	log.Println(fmt.Sprintf("File generated: %v\n\n", downReq.FileName))
 
 }
